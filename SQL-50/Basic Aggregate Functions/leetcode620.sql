0c2b0653b54491cbd3657d5d6f4a88c2b0633817
albertsingh <albert531singh@gmail.com>
2025-12-17 10:30:07 +0530

    Refactor SQL queries for Cinema and add average experience years calculation for projects

--- SQL-50/Basic Aggregate Functions/leetcode620.sql
+++ SQL-50/Basic Aggregate Functions/leetcode620.sql
@@ -1,2 +1,4 @@
---- Write your MySQL query statement below
-select * from cinema where description != "boring"  and mod(id , 2) <> 0 order by rating desc;
+# Write your MySQL query statement below
+select * from Cinema 
+where id % 2 =1 and description != 'boring'
+order by rating desc;
