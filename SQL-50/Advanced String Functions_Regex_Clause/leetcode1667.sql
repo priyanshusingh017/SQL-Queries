16bd57a7ea7c6d6353b73a63d685042554ebfca6
albertsingh <albert531singh@gmail.com>
2025-12-19 13:03:44 +0530

    Add SQL queries for leetcode problems 1527 and 1667

---
+++ SQL-50/Advanced String Functions_Regex_Clause/leetcode1667.sql
@@ -0,0 +1,8 @@
+--- Write your MySQL query statement below
+select user_id ,
+concat(
+    upper(substring(name,1,1)), -- upper/lower(string,postion,length)
+    lower(substring(name ,2,length(name)-1))
+)as name
+from Users
+order by user_id;
